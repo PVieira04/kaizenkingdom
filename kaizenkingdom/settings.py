048e25c7f2ce87360c469a252154323862db491a
PVieira04 <vieira.pjpv@gmail.com>
2023-08-25 13:40:14 +0000

    Change Allowed Hosts for Heroku

--- kaizenkingdom/settings.py
+++ kaizenkingdom/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kaizen-kingdom.herokuapp.com', '8000-pvieira04-kaizenkingdom-of13ftxz3w9.ws-us104.gitpod.io']
+ALLOWED_HOSTS = ['kaizen-kingdom-7933fecd6f94.herokuapp.com', '8000-pvieira04-kaizenkingdom-of13ftxz3w9.ws-us104.gitpod.io']
 
 
 # Application definition
