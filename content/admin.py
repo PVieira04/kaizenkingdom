76f597733ca0c5662b38904a4947ef97dca20f42
PVieira04 <vieira.pjpv@gmail.com>
2023-09-03 12:24:03 +0000

    Add models to admin panel and include summernote

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Course
+from .models import Unit
+from .models import Topic
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description')
+
+@admin.register(Unit)
+class UnitAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description')
+
+@admin.register(Topic)
+class TopicAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
