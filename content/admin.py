a15de43358fe3c8e47fcc57294dcd84cb36f3111
PVieira04 <vieira.pjpv@gmail.com>
2023-09-04 22:05:02 +0000

    Add more functionality to admin panel

--- content/admin.py
+++ content/admin.py
@@ -11,24 +11,44 @@ class CourseAdmin(SummernoteModelAdmin):
 class UnitAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('description')
+    fields = ('course', 'number', 'title', 'slug', 'description')
 
 @admin.register(Topic)
 class TopicAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
-    list_display = ('course_name', 'unit', 'number', 'title')
+    list_display = ('topic_name', 'unit_name', 'course_name')
+    ordering = ('unit__course__title', 'unit__unit_number', 'number') # This changes default sorting
+    fields = ('unit', 'number', 'title', 'slug', 'content')
+    search_fields = ['title', 'unit__course__title', 'unit__title']
+    list_filter = ('unit__course__title', 'unit__title')
+    
+    def course_name(self, obj):
+        return obj.unit.course.title
+
+    course_name.short_description = 'Course' # Custom column heading
+    course_name.admin_order_field = 'unit__course__title' # Enable sorting options
 
     def unit_number(self, obj):
         return obj.unit.number
     
-    def course_name(self, obj):
-        return obj.unit.course.title
+    def unit_name(self, obj):
+        return f"Unit {obj.unit.number}: {obj.unit.title}"
     
-    unit_number.short_description = 'Unit Number'  # Set a custom column header for unit_number
-    course_name.short_description = 'Course Name'  # Set a custom column header for course_name
+    unit_name.short_description = 'Unit'
+    unit_name.admin_order_field = 'unit__unit_number'
+    
+    def topic_name(self, obj):
+        return f"Topic {obj.number}: {obj.title}"
 
+    topic_name.short_description = 'Topic'
+    topic_name.admin_order_field = 'number'
+
+    # Override the get_queryset() function which runs every time django wants to display a list of topics from the database.
     def get_queryset(self, request):
+        # Retrieve the default queryset - this includes all topics in the database.
         queryset = super().get_queryset(request)
+        # Modify the queryset by ordering it in a specific way.
         queryset = queryset.order_by('unit__course__title', 'unit__unit_number', 'number')
         return queryset
 
