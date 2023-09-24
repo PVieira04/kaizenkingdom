b6fa219fabe465a08ef67acec2607ab6cc1f7ada
PVieira04 <vieira.pjpv@gmail.com>
2023-09-24 13:04:22 +0000

    Add paths for unit and topic in hierarchy

--- content/urls.py
+++ content/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.CourseList.as_view(), name='course_list'),
-    path('<slug:slug>/', views.CourseDetail.as_view(), name='course_detail'),
+    path('<slug:course_slug>/', views.CourseDetail.as_view(), name='course_detail'),
+    path('<slug:course_slug>/<slug:unit_slug>/', views.UnitDetail.as_view(), name='unit_detail'),
+    path('<slug:course_slug>/<slug:unit_slug>/<slug:topic_slug>/', views.TopicDetail.as_view(), name='topic_detail'),
 ]
