347e31f5f61c5fc3014a6fbd3c1f24f96903dc14
PVieira04 <vieira.pjpv@gmail.com>
2023-09-24 13:05:00 +0000

    Add List and Detail Views for rest of Models

--- content/views.py
+++ content/views.py
@@ -8,21 +8,48 @@ class CourseList(generic.ListView):
     paginate_by = 6
 
 class CourseDetail(View):
-    def get(self, request, slug, *args, **kwargs):
-        course = Course.objects.get(slug=slug)
+    def get(self, request, course_slug, *args, **kwargs):
+        course = get_object_or_404(Course, slug=course_slug)
         courses = Course.objects.all()
-        units = Unit.objects.filter(course__id=course.id)
-        return render(
-            request,
-            "course_detail.html",
-            {
-                "course": course,
-                "units": units,
-                "items": list(courses) + list(units)
-            }
-        )
+        units = Unit.objects.filter(course=course)
+        context = {
+            'course' : course,
+            'units' : units,
+        }
+        return render(request, "course_detail.html", context)
 
-class UnitDetail(generic.ListView):
+class UnitList(generic.ListView):
     model = Unit
     template_name = 'unit_list.html'
     paginate_by = 6
+
+class UnitDetail(View):
+    def get(self, request, course_slug, unit_slug, *args, **kwargs):
+        course = get_object_or_404(Course, slug=course_slug)
+        unit = get_object_or_404(Unit, slug=unit_slug, course=course)
+        units = Unit.objects.all()
+        topics = Topic.objects.filter(unit=unit)
+        context = {
+                "course" : course,
+                "unit" : unit,
+                "topics" : topics,
+            }
+        return render(request, "unit_detail.html", context)
+
+class TopicList(generic.ListView):
+    model = Topic
+    template_name = 'topic_list.html'
+    paginate_by = 6
+
+class TopicDetail(View):
+    def get(self, request, course_slug, unit_slug, topic_slug, *args, **kwargs):
+        course = get_object_or_404(Course, slug=course_slug)
+        unit = get_object_or_404(Unit, slug=unit_slug)
+        topic = get_object_or_404(Topic, slug=topic_slug, unit=unit)
+        topics = Topic.objects.all()
+        context = {
+                "topic" : topic,
+                "unit" : unit,
+                "course" : course,
+            }
+        return render(request, "topic_detail.html", context)
