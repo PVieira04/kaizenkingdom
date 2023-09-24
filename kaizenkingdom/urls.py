0e232cdaa0e4e8c179b4cf8b9e91756fd88edfed
PVieira04 <vieira.pjpv@gmail.com>
2023-09-24 15:25:29 +0000

    Change URLs to fix bug

--- kaizenkingdom/urls.py
+++ kaizenkingdom/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('content.urls'), name='content_urls'),
+    path('learn/', include('content.urls'), name='content_urls'),
     path('profile/', include('allauth.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
