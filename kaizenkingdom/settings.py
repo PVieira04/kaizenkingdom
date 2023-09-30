c998b7df90dddbb8bf9ff7b40bebd80f7d71323a
PVieira04 <vieira.pjpv@gmail.com>
2023-09-30 09:22:18 +0000

    Change settings.py for deployment

--- kaizenkingdom/settings.py
+++ kaizenkingdom/settings.py
@@ -27,7 +27,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+#DEBUG = os.environ.get('DEBUG', False)
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST')]
 
